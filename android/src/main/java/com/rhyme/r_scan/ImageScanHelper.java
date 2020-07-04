e9c2514cfda91a84b3447d5a6effddeb1ff2a6f0
masewo <masewo@gmx.net>
2020-07-04 15:20:01 +0200

    use default SSLSocketFactory to solve TrustManager security vulnerability reported by Google Play

--- android/src/main/java/com/rhyme/r_scan/ImageScanHelper.java
+++ android/src/main/java/com/rhyme/r_scan/ImageScanHelper.java
@@ -211,12 +211,7 @@ public class ImageScanHelper extends ContextWrapper {
                         HttpsURLConnection connection = (HttpsURLConnection) myUrl.openConnection();
                         connection.setReadTimeout(6 * 60 * 1000);
                         connection.setConnectTimeout(6 * 60 * 1000);
-                        TrustManager[] tm = {new MyX509TrustManager()};
-                        SSLContext sslContext = SSLContext.getInstance("TLS");
-                        sslContext.init(null, tm, new java.security.SecureRandom());
-                        // 从上述SSLContext对象中得到SSLSocketFactory对象
-                        SSLSocketFactory ssf = sslContext.getSocketFactory();
-                        connection.setSSLSocketFactory(ssf);
+                        connection.setSSLSocketFactory((SSLSocketFactory) SSLSocketFactory.getDefault());
                         connection.connect();
                         bitmap = BitmapFactory.decodeStream(connection.getInputStream());
                     } else {
@@ -263,21 +258,5 @@ public class ImageScanHelper extends ContextWrapper {
                 }
             }
         });
-    }
-
-    private class MyX509TrustManager implements X509TrustManager {
-
-        // 检查客户端证书
-        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-        }
-
-        // 检查服务器端证书
-        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-        }
-
-        // 返回受信任的X509证书数组
-        public X509Certificate[] getAcceptedIssuers() {
-            return null;
-        }
     }
 }
